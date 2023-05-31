538537fcfd1cd2c9c235f103faab3e0ebf51c25e
valeywzhang <valeywzhang@tencent.com>
2023-05-31 20:18:17 +0800

    feat: a simple implement of describe and it

---
+++ tests/circus.test.js
@@ -0,0 +1,11 @@
+describe('circus test', ()=>{
+    it('works', ()=>{
+        expect(true).toBe(true);
+    });
+});
+
+describe('second circus test', ()=>{
+    it(`dosen't work`, ()=>{
+        expect(true).toBe(false);
+    });
+})
--- worker.js
+++ worker.js
@@ -9,11 +9,29 @@ exports.runTest = async function (testFile) {
     }
 
     try {
+        const describeFns = [];
+        let currentDescribeFns;
+
+        const describe = (name, fn) => describeFns.push([name, fn]);
+        const it = (name, fn) => currentDescribeFns.push([name, fn]);
+
         // in .test.js, when call expect function, it can access the expect in this scope.
         eval(code);
+
+        for (const [name, fn] of describeFns) {
+            currentDescribeFns = [];
+            testName = name;
+            fn();
+            
+            for(const [name, fn] of currentDescribeFns){
+                testName += '  ' + name;
+                fn();
+            }
+        }
+
         testResult.success = true;
     } catch (e) {
-        testResult.errorMessage = e.message;
+        testResult.errorMessage = testName + ': ' + e.message;
     }
     // console.log(`worker id: ${process.env.JEST_WORKER_ID}\nfile: ${testFile}:\n${code}`);
     return testResult;
