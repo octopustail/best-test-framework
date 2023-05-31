7ad5a9863c78671c8ab8fc0fe1566e40df10d5c6
valeywzhang <valeywzhang@tencent.com>
2023-05-31 19:59:02 +0800

    feat: ✅ step3: using assertion framework and report result. receive secode auguement to run specific test file

--- index.mjs
+++ index.mjs
@@ -36,25 +36,36 @@ await hasteMap.setupCachePath(hasteMapOptions);
 const { hasteFS } = await hasteMap.build();
 
 // ⭐️ we can apply a set of globs to the in-memory representation of the file system instead of running actual file system operations:
-const testFiles = hasteFS.matchFilesWithGlob(['**/*.test.js']);
+const testFiles = hasteFS.matchFilesWithGlob([
+    // process.argv[2]: get the third argument in command;
+    process.argv[2] ? `**/${process.argv[2]}*` : '**/*.test.js'
+]);
 
 // read all the code in out test file
 // although we use Promise.all, but node is still single thread. So if we want to run test more efficiently, we may use workers thread.
 // due to the matter of set up a boilerplate of a worker thread, we can use jest-worker instead.
 const worker = new Worker(join(root, 'worker.js'));
 
+let hasFailed = false;
 await Promise.all(
     Array.from(testFiles).map(async (testFile) => {
 
         const { success, errorMessage } = await worker.runTest(testFile);
         const status = success ? chalk.green.inverse.bold(' PASS ')
             : chalk.red.inverse.bold(' FAIL ');
+
+
         console.log(status + ' ' + chalk.dim(relative(root, testFile)));
         if (!success) {
+            hasFailed = true;
             console.log('  ' + errorMessage);
         }
     })
 )
 
 worker.end();
+if(hasFailed){
+    console.log('\n' + chalk.red.bold('Test run failed, please fix all the failing tests.'));
 
+    process.exit(1);
+}
